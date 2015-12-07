358810003fa880cdc324a0ba32a84e2066bc7b84
Konstantin Kovalenko <red_nada@mail.ru>
2015-12-07 15:57:53 +0200

    using simple resources

--- src/main/java/work/savedata/PathToSaveOnHDD.java
+++ src/main/java/work/savedata/PathToSaveOnHDD.java
@@ -1,11 +1,26 @@
 package work.savedata;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import javax.annotation.PostConstruct;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
 @Component("path")
 public class PathToSaveOnHDD {
 
-    private String path = "testneeds";
+    private String path;
+
+    @PostConstruct
+    private void init() throws IOException {
+        ApplicationContext context = new AnnotationConfigApplicationContext();
+        Resource resource = context.getResource("pathToSave.txt");
+        BufferedReader in = new BufferedReader(new InputStreamReader(resource.getInputStream()));
+        path = in.readLine();
+    }
 
     public String getPath() {
         return path;
